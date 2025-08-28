2802bcfff576656d7f809572b551d4b246a33373
mayukhdeb <mayukhmainak2000@gmail.com>
2025-08-28 15:22:48 -0400

    enable all subjects

--- mosaic/__init__.py
+++ mosaic/__init__.py
@@ -35,25 +35,36 @@ def load(
     assert name in name_to_dataset_mapping.keys(), \
         f"Dataset name {name} is not valid. Please choose from {list(name_to_dataset_mapping.keys())}."
     
-    if isinstance(subject_id, str):
-        assert subject_id == "all", \
-            "If subject_id is a string, it must be 'all'."
+    if name != "all":
+        if isinstance(subject_id, str):
+            assert subject_id == "all", \
+                "If subject_id is a string, it must be 'all'."
+            return MultiSubjectDataset(
+                datasets=[
+                    name_to_dataset_mapping[name](
+                        folder=os.path.join(folder, name),
+                        subject_id=subject_id,
+                    )
+                    for subject_id in range(1, num_subjects[name]+1)
+                ]
+            )
+
+        else:
+            dataset_class = name_to_dataset_mapping[name]
+            folder = os.path.join(
+                folder, name
+            )
+            return dataset_class(
+                folder=folder,
+                subject_id=subject_id,
+            )
+    else:
+        assert subject_id == "all",\
+            "If name is 'all', subject_id must be 'all'."
         return MultiSubjectDataset(
             datasets=[
-                name_to_dataset_mapping[name](
-                    folder=os.path.join(folder, name),
-                    subject_id=subject_id,
-                )
+                name_to_dataset_mapping[name](folder=os.path.join(folder, name), subject_id=subject_id)
+                for name in name_to_dataset_mapping.keys()
                 for subject_id in range(1, num_subjects[name]+1)
             ]
-        )
-
-    else:
-        dataset_class = name_to_dataset_mapping[name]
-        folder = os.path.join(
-            folder, name
-        )
-        return dataset_class(
-            folder=folder,
-            subject_id=subject_id,
         )
