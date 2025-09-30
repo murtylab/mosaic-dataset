512a618d95754e8c03e3d47070459c625815fe76
mayukhdeb <mayukhmainak2000@gmail.com>
2025-09-30 15:07:45 -0400

    pass folder arg

--- mosaic/models/__init__.py
+++ mosaic/models/__init__.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 
 def from_pretrained(
     backbone_name: str = "resnet18",
-    vertices: Union[str, list] = "all",
+    vertices: Union[str, list] = "visual",
     framework: str = "multihead",
     subjects: Union[str, list] = "all",
+    folder: str = "./mosaic_models/",
 ) -> nn.Module:
     assert backbone_name in valid_backbone_names, \
         f"Invalid backbone_name {backbone_name}. Must be one of {valid_backbone_names}"
@@ -17,4 +18,5 @@ def from_pretrained(
         vertices=vertices,
         framework=framework,
         subjects=subjects,
+        folder=folder
     )
