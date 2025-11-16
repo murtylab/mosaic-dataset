cfaa660d3a6ea3eeda73e5b06619417ea810a6e3
mayukhdeb <mayukhmainak2000@gmail.com>
2025-11-16 13:02:57 -0500

    offload aws stuff

--- mosaic/datasets/timeseries/bold_moments.py
+++ mosaic/datasets/timeseries/bold_moments.py
@@ -1,35 +1,9 @@
 import os
 import requests
 from tqdm import tqdm
-import xml.etree.ElementTree as ET
 from ...constants import task_folder, BASE_URL
 from ...utils.download import download_file
-
-
-def list_s3_folder(base_url: str, prefix: str):
-    """
-    List all objects under a public S3 prefix by parsing its XML index.
-    Works for S3 static website endpoints like mosaicfmri.s3.amazonaws.com.
-    """
-    # S3 bucket listing URL
-    list_url = f"{base_url}?prefix={prefix}"
-
-    resp = requests.get(list_url)
-    resp.raise_for_status()
-
-    # Parse the XML list
-    root = ET.fromstring(resp.text)
-    namespace = {"s3": "http://s3.amazonaws.com/doc/2006-03-01/"}
-
-    keys = []
-    for contents in root.findall("s3:Contents", namespace):
-        key = contents.find("s3:Key", namespace).text
-        # Skip directory placeholders
-        if not key.endswith("/"):
-            keys.append(key)
-
-    return keys
-
+from ...utils.aws import list_s3_folder
 
 def download_bold_moments_timeseries_data(folder: str):
     """
